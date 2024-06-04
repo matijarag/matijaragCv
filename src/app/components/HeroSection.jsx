13fde635610ca05178be66f786eb1ca5d428cbe4
Matias Jara <mati.jarag@gmail.com>
2024-06-04 00:18:56 -0400

    fix_img_url

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import CatPc from "../../../public/images/cat_pc-removebg.png";
 
 const HeroSection = () => {
   return (
@@ -56,7 +57,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/cat_pc-removebg.png"
+              src={CatPc}
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={340}
